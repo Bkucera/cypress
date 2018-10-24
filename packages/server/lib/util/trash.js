63b2710c2e755426d5e80877bdec24647d488d3c
decaffeinate <brian.mann86@gmail.com>
2018-10-23 20:00:39 -0400

    decaffeinate: Rename app_data.coffee and 41 other files from .coffee to .js

--- packages/server/lib/util/trash.js
+++ packages/server/lib/util/trash.js
@@ -1,20 +1,14 @@
-const fs = require('./fs')
-const os = require('os')
-const path = require('path')
-const trash = require('trash')
-const Promise = require('bluebird')
+fs      = require("./fs")
+path    = require("path")
+trash   = require("trash")
+Promise = require("bluebird")
 
 module.exports = {
-  folder (pathToFolder) {
-    return fs.statAsync(pathToFolder)
-    .then(() => {
-      if (os.platform() === 'linux') {
-        return fs.emptyDir(pathToFolder)
-      }
-
-      return Promise.map(fs.readdirAsync(pathToFolder), (item) => {
-        return trash([path.join(pathToFolder, item)])
-      })
-    }).catch({ code: 'ENOENT' }, () => {})
-  },
+  folder: (pathToFolder) ->
+    fs.statAsync(pathToFolder)
+    .then ->
+      Promise.map(fs.readdirAsync(pathToFolder), (item) ->
+        return trash([path.join(pathToFolder, item)]))
+    .catch {code: "ENOENT"}, ->
+      return
 }
